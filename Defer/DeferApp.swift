fe1daf9f5c26d4a802c22bada38617d2d65db28b
JC Frane <jc.frane@gmail.com>
2026-02-07 18:18:32 +0800

    Initial Commit

---
+++ Defer/DeferApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeferApp.swift
+//  Defer
+//
+//  Created by JC Frane on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DeferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
